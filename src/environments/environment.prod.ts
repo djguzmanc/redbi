4eba2d3e889ba82bbb1c4a27f1d3c32daacaa265
Dave <djguzmanc@unal.edu.co>
2019-02-10 20:14:50 -0500

    heroku

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyA4NFFrMroT_uMXTEgYigQzUXtTiN9MDwU",
+    authDomain: "bikeapp-dbb93.firebaseapp.com",
+    databaseURL: "https://bikeapp-dbb93.firebaseio.com",
+    projectId: "bikeapp-dbb93",
+    storageBucket: "bikeapp-dbb93.appspot.com",
+    messagingSenderId: "1078619619628"
+  }
 };
