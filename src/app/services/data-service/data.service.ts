acf4e8eff3e40dd3eaa2f3d8dd3f03ca25025dcd
Dave <djguzmanc@unal.edu.co>
2019-02-27 14:50:09 -0500

    Verion 1.1.6

--- src/app/services/data-service/data.service.ts
+++ src/app/services/data-service/data.service.ts
@@ -15,7 +15,7 @@ export class DataService {
 
   globalLoading = new Subject<any>( )
 
-  version: string = '1.1.5'
+  version: string = '1.1.6'
 
   constructor( private afAuth: AngularFireAuth, private db: AngularFirestore, private alertService: AlertService ) {
     this.globalLoading.next( true )
