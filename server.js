eaed336d0b793ed2fd91e6fb722be3cde567d8dd
Dave <djguzmanc@unal.edu.co>
2019-03-02 22:46:36 -0500

    Deploy fix

--- server.js
+++ server.js
@@ -1,11 +1,9 @@
 const express = require( 'express' );
 const path = require( 'path' );
-const compression = require( 'compression' )
 
 const app = express( );
 
 app.use( express.static( __dirname + '/dist/redbi' ) );
-app.use( compression( ) );
 
 app.get( '/*', function( req, res ) {
   res.sendFile( path.join( __dirname + '/dist/redbi/index.html' ) );
