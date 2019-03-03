b4b402169a9f5fa1d13491e7793b3cf43be135ad
Dave <djguzmanc@unal.edu.co>
2019-03-02 22:38:46 -0500

    Server optimization

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require( 'express' );
 const path = require( 'path' );
+const compression = require( 'compression' )
 
 const app = express( );
 
+app.use( compression( ) );
 app.use( express.static( __dirname + '/dist/redbi' ) );
 
 app.get( '/*', function( req, res ) {
